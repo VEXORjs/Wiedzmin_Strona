0fe5cca9683c718ffff684d3cce35b5e71a0553f
VEXORteCHc <kacperadam10@gmail.com>
2022-05-04 00:43:08 +0200

    web scrapping part1

--- public/js/webscr.js
+++ public/js/webscr.js
@@ -1,1 +1,11 @@
-const URL = "https://pl.gear.cdprojektred.com/yennefer-the-kunoichi.html?forceRegion=pl&path=yennefer-the-kunoichi.html"
+const rp = require('require-promise');
+const url = "https://pl.gear.cdprojektred.com/yennefer-the-kunoichi.html?forceRegion=pl&path=yennefer-the-kunoichi.html"
+
+rp(url)
+    .then(function(html) {
+        //happens when the request goes well
+        console.log(html);
+    })
+    .catch(function(err) {
+        //handle an error when sth goes wrong 
+    });

27408fc2b447970292181b84adba8951d7ab0137
GHOSTY <kacperadam10@gmail.com>
2024-01-11 20:24:19 +0100

    Update login.php

--- public/login.php
+++ public/login.php
@@ -6,7 +6,7 @@ $password = $_POST['password'];
 // Tutaj należy dodać logikę weryfikacji użytkownika
 // Na potrzeby przykładu zakładamy, że istnieje użytkownik o nazwie "admin" i haśle "password"
 $poprawna_nazwa_uzytkownika = "admin";
-$poprawne_haslo = "password";
+$poprawne_haslo = "passwd";
 
 if ($username === $poprawna_nazwa_uzytkownika && $password === $poprawne_haslo) {
     // Użytkownik prawidłowy, można przekierować na inną stronę
@@ -14,6 +14,7 @@ if ($username === $poprawna_nazwa_uzytkownika && $password === $poprawne_haslo) 
     exit();
 } else {
     // Błędne dane logowania
+    header("Location: login_form.html");
     echo "Nieprawidłowa nazwa użytkownika lub hasło";
 }
 ?>
